ffcd9c6d9f23daf0a171e7527e796682fe9bb416
MikelNoppo <mikelnoppo03@gmail.com>
2025-07-24 18:33:18 +0800

    refactor: remove RainfallBarChart and SimpleRainfallBarChart exports from charts index

--- components/charts/index.ts
+++ components/charts/index.ts
@@ -1,5 +1,4 @@
 export { AreaChart } from './area-chart'
 export { MonthlyChart } from './monthly-chart'
-export { RainfallBarChart, SimpleRainfallBarChart } from './simple-rainfall-bar-chart'
 export { RainfallChart } from './rainfall-chart'
 export { RainfallClassificationChart, RainfallClassificationSummary } from './rainfall-classification-chart'
